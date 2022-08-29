d87c673d7d706f171338c617294b5fae6d89c16a
Andrei Soroka <androsor99@gmail.com>
2022-08-29 22:04:57 +0300

    BeanFactoryPostProcessor (BFPP)

--- src/main/java/com/androsor/ApplicationRunner.java
+++ src/main/java/com/androsor/ApplicationRunner.java
@@ -4,7 +4,10 @@ import com.androsor.database.pool.ConnectionPool;
 import com.androsor.database.repository.CompanyRepository;
 import com.androsor.ioc.Container;
 import com.androsor.service.UserService;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.Serializable;
 
 /**
  * The {@code ApplicationRunner}
@@ -16,14 +19,23 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationRunner {
 
     public static void main(String[] args) {
+
+        String value = "abracadabra";
+        System.out.println(CharSequence.class.isAssignableFrom(value.getClass()));
+        System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
+        System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
+
+
         try (var context = new ClassPathXmlApplicationContext("application.xml")) {
             //      clazz -> String -> Map<String, Object>
             var connectionPool = context.getBean("p1", ConnectionPool.class);
+            System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(connectionPool.getClass()));
             System.out.println(connectionPool);
             var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
             System.out.println(companyRepository);
         }
     }
 }
+
 
 
