6fee646c902e2031af51285bf34c1e1f4207f02b
Andrei Soroka <androsor99@gmail.com>
2022-08-29 11:02:18 +0300

    Constructor Injection

--- src/main/java/com/androsor/database/pool/ConnectionPool.java
+++ src/main/java/com/androsor/database/pool/ConnectionPool.java
@@ -1,5 +1,8 @@
 package com.androsor.database.pool;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * The {@code ConnectionPool}
  *
@@ -8,4 +11,19 @@ package com.androsor.database.pool;
  * @createDate 26.08.2022 22:43
  */
 public class ConnectionPool {
+
+    private final String username;
+    private final Integer poolSize;
+    private final List<Object> args;
+    private final Map<String, Object> properties;
+
+    public ConnectionPool(String username,
+                          Integer poolSize,
+                          List<Object> args,
+                          Map<String, Object> properties) {
+        this.username = username;
+        this.poolSize = poolSize;
+        this.args = args;
+        this.properties = properties;
+    }
 }
