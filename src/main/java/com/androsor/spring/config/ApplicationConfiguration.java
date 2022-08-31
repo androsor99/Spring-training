2f8461c6c46066bb5fa68cb025cf883bff9e826c
Andrei Soroka <androsor99@gmail.com>
2022-08-31 11:54:00 +0300

    @Bean. Часть 2

--- src/main/java/com/androsor/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/androsor/spring/config/ApplicationConfiguration.java
@@ -38,7 +38,17 @@ public class ApplicationConfiguration {
         }
 
         @Bean
+        public ConnectionPool pool3() {
+                return new ConnectionPool("test-pool", 25);
+        }
+
+        @Bean
         public UserRepository userRepository2(ConnectionPool pool2) {
                 return new UserRepository(pool2);
+        }
+
+        @Bean
+        public UserRepository userRepository3() {
+                return new UserRepository(pool3());
         }
 }
