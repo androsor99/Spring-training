2c399229d17f00ef6226c1c7d643b71485aea785
Andrei Soroka <androsor99@gmail.com>
2022-08-31 11:35:23 +0300

    @Bean. Часть 1

--- src/main/java/com/androsor/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/androsor/spring/config/ApplicationConfiguration.java
@@ -1,7 +1,11 @@
 package com.androsor.spring.config;
 
+import com.androsor.spring.database.pool.ConnectionPool;
 import com.androsor.spring.database.repository.CrudRepository;
+import com.androsor.spring.database.repository.UserRepository;
 import com.androsor.web.config.WebConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Component;
 
@@ -26,4 +30,15 @@ import static org.springframework.context.annotation.ComponentScan.Filter;
             @Filter(type = FilterType.REGEX, pattern = "com\\..Repository")
         })
 public class ApplicationConfiguration {
+
+        @Bean(name = "pool2")
+        @Scope(BeanDefinition.SCOPE_SINGLETON)
+        public ConnectionPool pool2(@Value("${db.username}") String username) {
+                return new ConnectionPool(username, 20);
+        }
+
+        @Bean
+        public UserRepository userRepository2(ConnectionPool pool2) {
+                return new UserRepository(pool2);
+        }
 }
