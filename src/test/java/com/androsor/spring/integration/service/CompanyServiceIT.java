1792256124f0e09375a69f8eb7d175a340a2f6f8
Andrei Soroka <androsor99@gmail.com>
2022-09-05 11:43:55 +0300

    Integration Testing. Part 2

---
+++ src/test/java/com/androsor/spring/integration/service/CompanyServiceIT.java
@@ -0,0 +1,52 @@
+package com.androsor.spring.integration.service;
+
+import com.androsor.spring.ApplicationRunner;
+import com.androsor.spring.database.entity.Company;
+import com.androsor.spring.dto.CompanyReadDto;
+import com.androsor.spring.listener.entity.EntityEvent;
+import com.androsor.spring.service.CompanyService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.ContextIdApplicationContextInitializer;
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+/**
+ * The {@code CompanyServiceIT}
+ *
+ * @author Andrei Soroka
+ * @version 1.0
+ * @createDate 05.09.2022 11:17
+ */
+
+@SpringBootTest
+//@ExtendWith(SpringExtension.class)
+//@ContextConfiguration(classes = ApplicationRunner.class, initializers = ConfigDataApplicationContextInitializer.class)
+public class CompanyServiceIT {
+
+    private static final Integer COMPANY_ID = 1;
+    @Autowired
+    private CompanyService companyService;
+
+    @Test
+    void findById() {
+        var actualResult = companyService.findById(COMPANY_ID);
+
+        assertTrue(actualResult.isPresent());
+
+        var expectedResult = new CompanyReadDto(COMPANY_ID);
+        actualResult.ifPresent(actual -> assertEquals(expectedResult, actual));
+    }
+}
