9fe67ef22005c0d6b4841a8375db4a9c6ff4f1d5
Andrei Soroka <androsor99@gmail.com>
2022-09-05 12:14:30 +0300

    Integration Testing. Part 3

--- src/test/java/com/androsor/spring/integration/service/CompanyServiceIT.java
+++ src/test/java/com/androsor/spring/integration/service/CompanyServiceIT.java
@@ -1,17 +1,22 @@
 package com.androsor.spring.integration.service;
 
 import com.androsor.spring.ApplicationRunner;
+import com.androsor.spring.config.DataBaseProperties;
 import com.androsor.spring.database.entity.Company;
 import com.androsor.spring.dto.CompanyReadDto;
+import com.androsor.spring.integration.annatation.IT;
 import com.androsor.spring.listener.entity.EntityEvent;
 import com.androsor.spring.service.CompanyService;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.ContextIdApplicationContextInitializer;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestConstructor;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
@@ -30,21 +35,24 @@ import static org.mockito.Mockito.verifyNoMoreInteractions;
  * @version 1.0
  * @createDate 05.09.2022 11:17
  */
-
-@SpringBootTest
+@IT
+@RequiredArgsConstructor
+//@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 //@ExtendWith(SpringExtension.class)
 //@ContextConfiguration(classes = ApplicationRunner.class, initializers = ConfigDataApplicationContextInitializer.class)
 public class CompanyServiceIT {
 
     private static final Integer COMPANY_ID = 1;
-    @Autowired
-    private CompanyService companyService;
+
+    private final CompanyService companyService;
+    private final DataBaseProperties dataBaseProperties;
 
     @Test
     void findById() {
         var actualResult = companyService.findById(COMPANY_ID);
 
         assertTrue(actualResult.isPresent());
+
 
         var expectedResult = new CompanyReadDto(COMPANY_ID);
         actualResult.ifPresent(actual -> assertEquals(expectedResult, actual));
