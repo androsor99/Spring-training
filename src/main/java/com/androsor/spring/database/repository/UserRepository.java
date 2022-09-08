d2d4c20548b4d9471a6607d89ffb07be8e8a8bc9
Andrei Soroka <androsor99@gmail.com>
2022-09-08 10:50:12 +0300

    @Modifying

--- src/main/java/com/androsor/spring/database/repository/UserRepository.java
+++ src/main/java/com/androsor/spring/database/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.androsor.spring.database.repository;
 
+import com.androsor.spring.database.entity.Role;
 import com.androsor.spring.database.entity.User;
 import com.androsor.spring.database.pool.ConnectionPool;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -28,6 +30,12 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value = "SELECT * FROM users u WHERE u.username = :username",
             nativeQuery = true)
     List<User> findAllByUsername(String username);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query(value = "update User u " +
+            "set u.role = :role " +
+            "where u.id in (:ids)")
+    int updateRole(Role role,Long... ids);
 
 
 }
--- src/test/java/com/androsor/spring/integration/database/repository/UserRepositoryTest.java
+++ src/test/java/com/androsor/spring/integration/database/repository/UserRepositoryTest.java
@@ -1,14 +1,18 @@
 package com.androsor.spring.integration.database.repository;
 
+import com.androsor.spring.database.entity.Role;
 import com.androsor.spring.database.entity.User;
 import com.androsor.spring.database.repository.UserRepository;
 import com.androsor.spring.integration.annatation.IT;
 import lombok.RequiredArgsConstructor;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.util.AssertionErrors;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @IT
@@ -18,9 +22,22 @@ class UserRepositoryTest {
     private final UserRepository userRepository;
 
     @Test
+    void checkUpdate() {
+        var user = userRepository.getById(1L);
+        assertSame(user.getRole(), Role.ADMIN);
+        user.setBirthDate(LocalDate.now());
+
+        var resultCount = userRepository.updateRole(Role.USER, 1L, 5L);
+        assertEquals(2, resultCount);
+
+        var theSameUser = userRepository.getById(1L);
+        assertSame(theSameUser.getRole(), Role.USER);
+    }
+
+    @Test
     void findAllBy() {
         var users = userRepository.findAllBy("a", "ov");
-        Assertions.assertThat(users).hasSize(3);
+        assertThat(users).hasSize(3);
         System.out.println(users);
     }
 }
