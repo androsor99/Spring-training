dd0ef26b8f113edb79c70005a2abd982e4fe97e9
Andrei Soroka <androsor99@gmail.com>
2022-09-15 11:46:00 +0300

    Parameters, Headers, Cookies

--- src/main/java/com/androsor/spring/http/controller/GreetingController.java
+++ src/main/java/com/androsor/spring/http/controller/GreetingController.java
@@ -1,10 +1,17 @@
 package com.androsor.spring.http.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * The {@code GreetingController}
@@ -18,13 +25,20 @@ import org.springframework.web.servlet.ModelAndView;
 public class GreetingController {
 
 //    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    @GetMapping("/hello")
-    public ModelAndView hello(ModelAndView modelAndView) {
+    @GetMapping("/hello/{id}")
+    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request,
+                              @RequestParam(value = "age", required = false) Integer age,
+                              @RequestHeader("accept") String accept,
+                              @CookieValue("JSESSIONID") String jSessionId,
+                              @PathVariable("id") Integer id) {
+        var ageParameterValue = request.getParameter("age");
+        var acceptHeader = request.getHeader("accept");
+        var cookies = request.getCookies();
         modelAndView.setViewName("greeting/hello");
         return modelAndView;
     }
 
-    @GetMapping("bye")
+    @GetMapping("/bye")
     public ModelAndView bye(ModelAndView modelAndView) {
         modelAndView.setViewName("greeting/bye");
         return modelAndView;
