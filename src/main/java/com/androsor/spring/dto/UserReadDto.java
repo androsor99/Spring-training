1c6e3c6ae42ebf0760d1ad25266a78ca7dd5d392
Andrei Soroka <androsor99@gmail.com>
2022-09-15 12:10:50 +0300

    Model

---
+++ src/main/java/com/androsor/spring/dto/UserReadDto.java
@@ -0,0 +1,17 @@
+package com.androsor.spring.dto;
+
+import lombok.Value;
+
+/**
+ * The {@code UserReadDto}
+ *
+ * @author Andrei Soroka
+ * @version 1.0
+ * @createDate 15.09.2022 11:53
+ */
+@Value
+public class UserReadDto {
+
+    Long id;
+    String username;
+}
--- src/main/java/com/androsor/spring/http/controller/GreetingController.java
+++ src/main/java/com/androsor/spring/http/controller/GreetingController.java
@@ -1,5 +1,6 @@
 package com.androsor.spring.http.controller;
 
+import com.androsor.spring.dto.UserReadDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.Cookie;
@@ -22,11 +25,19 @@ import javax.servlet.http.HttpServletRequest;
  */
 @Controller
 @RequestMapping("/api/v1")
+@SessionAttributes("user")
 public class GreetingController {
 
 //    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @GetMapping("/hello")
+    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request) {
+        modelAndView.setViewName("greeting/hello");
+        modelAndView.addObject("user", new UserReadDto(1L, "Ivan"));
+        return modelAndView;
+    }
+
     @GetMapping("/hello/{id}")
-    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request,
+    public ModelAndView hello2(ModelAndView modelAndView, HttpServletRequest request,
                               @RequestParam(value = "age", required = false) Integer age,
                               @RequestHeader("accept") String accept,
                               @CookieValue("JSESSIONID") String jSessionId,
@@ -39,7 +50,8 @@ public class GreetingController {
     }
 
     @GetMapping("/bye")
-    public ModelAndView bye(ModelAndView modelAndView) {
+    public ModelAndView bye(ModelAndView modelAndView,
+                            @SessionAttribute("user") UserReadDto user) {
         modelAndView.setViewName("greeting/bye");
         return modelAndView;
     }
