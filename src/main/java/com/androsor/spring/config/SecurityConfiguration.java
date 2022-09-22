28926247cf87e76d32bbf0347105c374ada0f09b
Andrei Soroka <androsor99@gmail.com>
2022-09-22 23:21:17 +0300

    Logout

--- src/main/java/com/androsor/spring/config/SecurityConfiguration.java
+++ src/main/java/com/androsor/spring/config/SecurityConfiguration.java
@@ -34,6 +34,10 @@ public class SecurityConfiguration {
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
 //                .httpBasic(Customizer.withDefaults());
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login")
+                        .deleteCookies("JSESSIONID"))
                 .formLogin(login -> login
                         .loginPage("/login")
                         .defaultSuccessUrl("/users")
