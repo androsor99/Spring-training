7a38524050f33dc54d59622cbbfb398b66aaa43b
Andrei Soroka <androsor99@gmail.com>
2022-09-26 22:49:08 +0300

    i18n. Thymeleaf

--- src/main/java/com/androsor/spring/config/WebConfiguration.java
+++ src/main/java/com/androsor/spring/config/WebConfiguration.java
@@ -1,9 +1,16 @@
 package com.androsor.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.convert.Jsr310Converters;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
 
 /**
  * The {@code WebConfiguration}
@@ -15,8 +22,29 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
+
+
     @Override
     public void addFormatters(FormatterRegistry registry) {
 //        registry.addConverter(Jsr310Converters.StringToLocalDateConverter.INSTANCE);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        var interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        return interceptor;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        var localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
     }
 }
